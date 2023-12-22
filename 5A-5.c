aa0a2f36c8896dac98964c6396ec1480b7c49296
Yuhf <existfuture@qq.com>
2023-12-22 14:23:02 +0800

    5A-5

--- 5A-5.c
+++ 5A-5.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("请输入正整数：")
+    int n,i,j;
+    printf("请输入正整数：");
     scanf("%d",&n);
+    printf("请输入%d个整数并都以回车结束:",n*n);
+    int a[6][6]={0};
+    for(i=0,j=0;i<=n-1;i++)
+    {
+        for(j=0;j<=n-1;j++)
+        scanf("%d",&a[i][j]);
+    }
+    int L,W,H;
+    for(i=0,L=0;i<=n-1;i++)
+    {
+        L+=a[i][n-1];
+    }
+    for(i=0,W=0;i<=n-2;i++)
+    {
+        W+=a[n-1][i];
+    }
+    for(i=n-2,H=0;i>=1;i--)
+    {
+        H+=a[n-1-i][i];
+    }
+    int sum;
+    for(i=0,j=0,sum=0;i<=n-1;i++)
+    {
+        for(j=0;j<=n-1;j++)
+        sum+=a[i][j];
+    }
+    int end;
+    end=sum-L-W-H;
+    printf("结果等于=%d",end);
+    return 0;
 }
