bce89ae1b2919905dd08d2564f96496223e5f2d0
Yuhf <existfuture@qq.com>
2023-12-22 14:27:49 +0800

    5A-5带注释

--- 5A-5.c
+++ 5A-5.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+    //以二维数组存储矩阵数据
     int n,i,j;
     printf("请输入正整数：");
     scanf("%d",&n);
@@ -11,6 +12,7 @@ int main()
         for(j=0;j<=n-1;j++)
         scanf("%d",&a[i][j]);
     }
+    //分别计算最后一行，列与副对角线的和，并不使之重复，最后用总和减去
     int L,W,H;
     for(i=0,L=0;i<=n-1;i++)
     {
@@ -24,6 +26,7 @@ int main()
     {
         H+=a[n-1-i][i];
     }
+    //总和的计算
     int sum;
     for(i=0,j=0,sum=0;i<=n-1;i++)
     {
