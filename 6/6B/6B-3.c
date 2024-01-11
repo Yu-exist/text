a1ade6400f672856457cf0910ff90465090b0205
Yuhf <existfuture@qq.com>
2024-01-11 21:02:56 +0800

    6B注释

--- 6/6B/6B-3.c
+++ 6/6B/6B-3.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+//定义结构体
 struct student 
 {
     char *num;
@@ -18,7 +19,9 @@ int main()
     {
         a[i].num=(char*)malloc(40*sizeof(char));
     }
+    //赋值文件指针
     FILE *p;
+    //输入内容
     printf("请输入学号，姓名，数学，语文，英语，并以空格分隔:");
     for(i=0;i<=4;i++)
     scanf("%s %s %d %d %d",a[i].num,a[i].name,&a[i].math,&a[i].chi,&a[i].Eng);
@@ -27,12 +30,13 @@ int main()
         a[i].all=a[i].math+a[i].chi+a[i].Eng;
         a[i].ave=a[i].all/3;
     }
-    
+    //写入文件
     p=fopen("D:/File setting/a.txt","w");
-    fprintf(p,"    学号        姓名    数学  语文  英语  总成绩  平均分  \n");
+    fprintf(p,"  学号   姓名    数学  语文  英语  总成绩  平均分  \n");
     for(i=0;i<=4;i++)
-    fprintf(p,"%s  %s    %d    %d    %d     %d         %d\n",a[i].num,a[i].name,a[i].math,a[i].chi,a[i].Eng,a[i].all,a[i].ave);
+    fprintf(p,"%s  %s    %d    %d    %d     %d      %d\n",a[i].num,a[i].name,a[i].math,a[i].chi,a[i].Eng,a[i].all,a[i].ave);
     fclose(p);
+    //读取文件，重新输出
     p=fopen("D:/File setting/a.txt","r");
     char print;
     while((print=getc(p))!=EOF)
--- 6/6B/6B-4.c
+++ 6/6B/6B-4.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main()
 {
+    //定义两个指针定位两个文件
     FILE *p;
     FILE *p1;
     p=fopen("D:/File setting/a.txt","r");
     p1=fopen("D:/File setting/b.txt","r");
+    //定义整型来存储行列值
     char p_a;
     char p1_a;
     int p_m,p_n;
@@ -13,16 +15,19 @@ int main()
     int p1_m,p1_n;
     p1_m=1;
     p1_n=0;
+    //循环遍历两文件中每一个元素
     while(( p_a=getc(p))!=EOF&&(p1_a=getc(p1))!=EOF)
     {
         p_n++;
         p1_n++;
+        //选择语句判断相等与否
         if(p_a!=p1_a)
         {
             printf("不同字符分别为:%c,%c",p_a,p1_a);
             printf("两文件行列值:行:%d 列:%d,行:%d 列:%d",p_m,p_n,p1_m,p1_n);
             break;
         }
+        //换行符进行累加和列数重置
         if(p_a=='\n')
         {
             p_m++;
